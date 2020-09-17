bf300846d32bac3b9a16849cf188d351560358cb
SGiuri <s.giuri@issm.it>
2020-09-17 23:23:53 +0200

    P1

--- isbn_verifier.py
+++ isbn_verifier.py
@@ -1,2 +1,12 @@
 def is_valid(isbn):
+    # 3-598-21508-8
+    isbn_cypre = []
+    for number in isbn:
+        if number.isdecimal():
+            isbn_cypre.append(int(number))
+    if isbn[-1] == "X":
+        isbn_cypre.append((10))
+    print(isbn_cypre)
     pass
+
+is_valid("3-598-21508-8")
